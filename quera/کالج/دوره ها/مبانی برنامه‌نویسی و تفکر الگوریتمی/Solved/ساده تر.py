06514ddbe2e2d384590655222fa5ee2e01dd9232
Mahdi-Amini <92228858+MahdiAmini12@users.noreply.github.com>
2023-03-27 04:14:41 +0330

    Create ساده تر.py

---
+++ quera/کالج/دوره ها/مبانی برنامه‌نویسی و تفکر الگوریتمی/Solved/ساده تر.py
@@ -0,0 +1,14 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+Sum = a + b + c + d 
+Average = Sum / 4
+Product = a * b * c * d
+Max = max(a, b, c, d)
+Min = min(a, b, c, d)
+print('Sum : %.6f' %(Sum))
+print('Average : %.6f' %(Average))
+print('Product : %.6f' %(Product))
+print('Max : %.6f' %(Max))
+print('Min : %.6f' %(Min))
